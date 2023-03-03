6c5c60f7caae963013bd050cf4fac9e5ab4f2cec
lea_chlt <lea.choulot@gmail.com>
2023-03-03 09:51:39 +0100

    Ce fichier contient les explications des deux méthodes analyserRequete et comparerDonnees

--- System_hospitalier.java
+++ System_hospitalier.java
@@ -35,11 +35,13 @@ public class System_hospitalier {
 	
 	private String analyserRequete(Requête requête) {
 		// Analyser la requête
+		// Cette méthode renvoie le texte "requête analysée"
 		return "requête analysée";
 	}
 	
 	private String comparerDonnes() {
 		// Comparaison des données
+		// Cette méthode renvoie le texte "Cluster trouvé"
 		return "Cluster trouvé";
 	}
 
